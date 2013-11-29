2b76ba69e1d6a6032ab6836a0b4d1d82cf2521f3
fjsandov <franciscosandoval15@gmail.com>
2013-11-29 14:22:36 -0300

    unit test OK

--- app/models/lodge.rb
+++ app/models/lodge.rb
@@ -1,13 +1,24 @@
 class Lodge < ActiveRecord::Base
+  has_many :comments
 
   def average_quality
-    #TODO
-    0
+    avg_quality = 0
+    count = 0
+    self.comments.each do |comment|
+      avg_quality += comment.quality
+      count += 1
+    end
+    count == 0 ? 0 : ((avg_quality/count.to_f)*100).round / 100.to_f
   end
 
 
   def average_price
-    #TODO
-    0
+    avg_price = 0
+    count = 0
+    self.comments.each do |comment|
+      avg_price += comment.price
+      count += 1
+    end
+    count == 0 ? 0 : ((avg_price/count.to_f)*100).round / 100.to_f
   end
 end
--- test/models/lodge_test.rb
+++ test/models/lodge_test.rb
@@ -4,13 +4,23 @@ class LodgeTest < ActiveSupport::TestCase
   test 'average_quality_range' do
     lodge = Lodge.first
     average_quality = lodge.average_quality
-    assert average_quality >= 1 and average_quality <= 5
+    if lodge.comments.size != 0
+      assert (average_quality >= 1 and average_quality <= 5),
+             "average quality must be between 1 and 5 but is #{average_quality}"
+    else
+      assert average_quality == 0, 'average quality must be 0 because lodge does not have comments'
+    end
   end
 
   test 'average_price_range' do
     lodge = Lodge.first
     average_price = lodge.average_price
-    assert average_price >= 1 and average_price <= 5
+    if lodge.comments.size != 0
+      assert (average_price >= 1 and average_price <= 5),
+             "average price must be between 1 and 5 but is #{average_price}"
+    else
+      assert average_price == 0, 'average price must be 0 because lodge does not have comments'
+    end
   end
 
   test 'average_price' do
@@ -18,8 +28,8 @@ class LodgeTest < ActiveSupport::TestCase
     Comment.create(lodge_id: lodge.id, quality: 3, price: 2)
     Comment.create(lodge_id: lodge.id, quality: 5, price: 3)
     Comment.create(lodge_id: lodge.id, quality: 5, price: 4)
-
-    asset lodge.average_quality == (3+5+5)/3
+    avg_price = (((2+3+4)/3.to_f)*100).round / 100.to_f
+    assert lodge.average_price == avg_price, "average price must be #{avg_price} and is #{lodge.average_price}"
   end
 
   test 'average_quality' do
@@ -27,7 +37,8 @@ class LodgeTest < ActiveSupport::TestCase
     Comment.create(lodge_id: lodge.id, quality: 3, price: 2)
     Comment.create(lodge_id: lodge.id, quality: 5, price: 3)
     Comment.create(lodge_id: lodge.id, quality: 5, price: 4)
-    assert lodge.average_price == (2+3+4)/3
+    avg_quality = (((3+5+5)/3.to_f)*100).round / 100.to_f
+    assert lodge.average_quality == avg_quality, "average quality must be #{avg_quality} and is #{lodge.average_quality}"
   end
 
 end
