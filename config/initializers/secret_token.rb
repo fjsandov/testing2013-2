ef2f536d208542f68ca68ffc5dbf69bddfc3a6b4
Matias Iordache <sindarghost@gmail.com>
2013-11-29 09:59:05 -0300

    secret ql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mochileros::Application.config.secret_key_base = '8015ccd19ec0024ced8e23ede852d3d7b3d125136082043cafadba70421261307c8e09a9811d9a311b1cf2c4b27ad0f73eba511ae6d64b8373135b45fea01758'
